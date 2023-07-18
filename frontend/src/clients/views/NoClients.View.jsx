7bc8053c28e187914441de3039d907282c3ba14a
NanasoDev <mariano.ceballos@gmail.com>
2023-07-18 20:25:09 -0300

    Fix: Change size of error page icon

--- frontend/src/clients/views/NoClients.View.jsx
+++ frontend/src/clients/views/NoClients.View.jsx
@@ -2,11 +2,9 @@ import { FaUserSlash } from 'react-icons/fa';
 
 export const NoClientsView = () => {
   return (
-    <div className="vh-100 d-flex flex-column justify-content-center align-items-center">
-
-        <FaUserSlash className=''/>
-        <h3>No hay clientes cargados</h3>
-
-    </div>
-  )
+    <div className="container vh-100 d-flex flex-column justify-content-center align-items-center">
+      <FaUserSlash style={{width: 50, height:50}}/>
+      <h3>No hay clientes cargados</h3>
+    </div>
+  )
 }
