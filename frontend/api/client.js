5c64f626dede30485966eafef435ec1834c66d46
NanasoDev <mariano.ceballos@gmail.com>
2023-07-18 15:01:47 -0300

    Add: Handle errors refactor

--- frontend/api/client.js
+++ frontend/api/client.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import Swal from 'sweetalert2';
+
 
 const publicClient = axios.create( {
     baseURL: import.meta.env.VITE_REACT_APP_API_BASE_URL,
@@ -8,49 +10,102 @@ const publicClient = axios.create( {
 } );
 
 const publicGET = async ( path ) => {
-    try {
-        const res = await publicClient.get( path );
-        if ( res.data && res.status === 200 ) {
-            return res;
-        }
-    } catch ( error ) {
-        throw new Error( error );
+  try {
+      const res = await publicClient.get( path );
+      if ( res.data && res.status === 200 ) {
+          return res;
+      }
+  } catch ( error ) {
+    if (error.code === 'ERR_NETWORK') {
+      return Swal.fire(
+        'Error!',
+        'Se produjo un error de conexión, por favor reintente nuevamente',
+        'error'
+      );
     }
+    if (error.response.status === 404) {
+      return Swal.fire(
+        'Error!',
+        'No se encontro el recurso solicitado.',
+        'error'
+      );
+    }
+    throw new Error( error );
+  }
 };
 
 const publicPOST = async ( path, data ) => {
   try {
-      const res = await publicClient.post( path, data );
-      if ( res.data && res.status === 200 || 201 ) {
-          return res;
-      }
+    const res = await publicClient.post( path, data );
+    if ( res.data && res.status === 200 || 201 ) {
+        return res;
+    }
   } catch ( error ) {
-      throw new Error( error );
+    if (error.code === 'ERR_NETWORK') {
+      return Swal.fire(
+        'Error!',
+        'Se produjo un error de conexión, por favor reintente nuevamente',
+        'error'
+      );
+    }
+    if (error.code === 'ERR_BAD_REQUEST') {
+      return Swal.fire(
+        'Error!',
+        'Se produjo un error, por favor verifique los datos ingresados',
+        'error'
+      );
+    }
+    throw new Error( error );
   }
 };
 
 const publicPATCH = async ( path, data ) => {
   try {
-      const res = await publicClient.patch( path, data );
-      if ( res.data && res.status === 200 ) {
-          return res;
-      }
+    const res = await publicClient.patch( path, data );
+    if ( res.data && res.status === 200 ) {
+        return res;
+    }
   } catch ( error ) {
-      throw new Error( error );
+    if (error.code === 'ERR_NETWORK') {
+      return Swal.fire(
+        'Error!',
+        'Se produjo un error de conexión, por favor reintente nuevamente',
+        'error'
+      );
+    }
+    if (error.code === 'ERR_BAD_REQUEST') {
+      return Swal.fire(
+        'Error!',
+        'Se produjo un error, por favor verifique los datos ingresados',
+        'error'
+      );
+    }
+    throw new Error( error );
   }
 };
 
 const publicDELETE = async ( path ) => {
   try {
-      const res = await publicClient.delete( path );
-      if ( res.data && res.status === 200 ) {
-          return res;
-      }
-  } catch ( error ) {
-    if ( error.code === 'ERR_BAD_REQUEST') {
-        return { status: 404 }
+    const res = await publicClient.delete( path );
+    if ( res.data && res.status === 200 ) {
+        return res;
     }
-    // throw new Error( error );
+  } catch ( error ) {
+    if (error.code === 'ERR_NETWORK') {
+      return Swal.fire(
+        'Error!',
+        'Se produjo un error de conexión, por favor reintente nuevamente',
+        'error'
+      );
+    }
+    if (error.code === 'ERR_BAD_REQUEST') {
+      return Swal.fire(
+        'Error!',
+        'Se produjo un error, por favor verifique los datos ingresados',
+        'error'
+      );
+    }
+    throw new Error( error );
   }
 };
 
