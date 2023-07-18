7937170e4ef4fdb90c2802357d01e6784b44daa4
NanasoDev <mariano.ceballos@gmail.com>
2023-07-18 20:26:02 -0300

    Add: search input to navbar

--- frontend/src/ui/Navbar.jsx
+++ frontend/src/ui/Navbar.jsx
@@ -1,13 +1,35 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useContext } from 'react';
+import { getClientByDni } from '../../api/client';
+import { ClientsContext } from '../context/ClientsContext';
+
 
 export const Navbar = () => {
+
+  const { register, handleSubmit, setValue } = useForm();
+  const { setSearchedClient, isLoading, setIsLoading } = useContext( ClientsContext );
+
+  const onSubmit = async ( { dni } ) => {
+    setIsLoading( true );
+    if ( !dni ) {
+      setIsLoading( false );
+      return setSearchedClient([]);
+    }
+    const { data } = await getClientByDni( dni );
+    if ( data ) {
+      setValue('dni', '');
+      setSearchedClient(data);
+      setIsLoading( false );
+    }
+    setValue('dni', '');
+    setIsLoading(false);
+  }
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-2">
+    <div className="container-fluid">
       <a className="navbar-brand" href="#">Clients App</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
         <ul className="navbar-nav">
           <li className="nav-item active">
             <Link className="nav-link" to={`/`}>Listado de clientes</Link>
@@ -16,7 +38,25 @@ export const Navbar = () => {
             <Link reloadDocument className="nav-link" to={`/create`}>Agregar cliente</Link>
           </li>
         </ul>
+        <form onSubmit={handleSubmit(onSubmit)} className="d-flex" role="search">
+          <input 
+            name='dni'
+            {...register("dni")} 
+            className="form-control me-2" 
+            type="number" 
+            placeholder="Buscar por DNI"
+          />
+            { isLoading ? (
+            <button class="btn btn-outline-info" disabled>
+              {/* <span class="spinner-border spinner-border-sm"></span> */}
+              Cargando...
+            </button>
+            ) : (
+              <button className="btn btn-outline-info" type="submit">Buscar</button>
+            )}
+      </form>
       </div>
+      
     </nav>
   )
 }
