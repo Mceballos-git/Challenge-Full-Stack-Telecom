7b4a72ae60a43e5a6265663cb7e50fc111c2e592
NanasoDev <mariano.ceballos@gmail.com>
2023-07-18 15:07:31 -0300

    Add: updateClient & genders & IsCreating flag

--- frontend/src/clients/views/AddEditClientView.jsx
+++ frontend/src/clients/views/AddEditClientView.jsx
@@ -3,63 +3,76 @@ import { useForm } from "react-hook-form";
 import { useNavigate, useParams } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import { ClientsContext } from '../../context/ClientsContext';
-import { createNewClient, deleteClient, getClientById } from '../../../api/client';
+import { createNewClient, deleteClient, getClientById, updateClient } from '../../../api/client';
 
 
 export const AddEditClientView = () => {
 
-    const { deleteClientContext, addClientContext } = useContext( ClientsContext );
-    const { register, handleSubmit, setValue, reset} = useForm();
-    const [ isLoading, setIsLoading ] = useState(false);
-    const navigate = useNavigate();
+    const { deleteClientContext, addClientContext, editClientContext } = useContext( ClientsContext );
+    const { register, handleSubmit, setValue} = useForm();
+    const navigate = useNavigate();    
     const { id } = useParams();
+    const isCreateMode = !id; // agrego variable para identificar si es creacion o edicion de cliente   
+
+
+    const [ isLoading, setIsLoading ] = useState(false);
+    const [ title, setTitle ] = useState('Agregar Cliente');
+
+    const validGenders = ['masculino', 'femenino', 'otro'];
     
     const onSubmit = async (data) => {
+      setIsLoading(true);
 
-      const toSend ={
-        ...data,
-        dni: parseInt(data.dni),
-        gender: data.gender.toLowerCase()
+      const dataToSend ={
+          ...data,
+          dni: parseInt(data.dni),
+          gender: data.gender.toLowerCase()
+        }
+
+      if(isCreateMode){       
+
+        console.log(dataToSend);
+
+        const resp = await createNewClient( dataToSend );
+
+        if( resp.status === 201 ) {
+          addClientContext( resp.data );
+          await Swal.fire({
+            title:'Exito!',
+            text:'El cliente fue creado correctamente',
+            icon:'success',
+            confirmButtonColor: 'grey',
+            confirmButtonText: 'OK',
+          });
+          setIsLoading(false);
+          navigate('/');
+        } else {
+          setIsLoading(false);
+        }
+      }else{      
+      
+        const resp = await updateClient( id, dataToSend );
+        if( resp.status === 200 ) {
+          editClientContext( parseInt(id), dataToSend );
+          await Swal.fire({
+            title:'Exito!',
+            text:'El cliente fue editado correctamente',
+            icon:'success',
+            confirmButtonColor: 'grey',
+            confirmButtonText: 'OK',
+          });
+          setIsLoading(false);
+          navigate('/');
+        } else {
+          setIsLoading(false);
+        }
+
       }
-
-
-      const resp = await createNewClient( toSend );
-      console.log(resp);
-      if ( !resp ) {
-        setIsLoading(false);
-        return Swal.fire(
-          'Error!',
-          'Se produjo un error de conexión, por favor reintente nuevamente',
-          'error'
-        );
-      }
-      if( resp.status === 201 ) {
-        addClientContext( resp.data );
-        await Swal.fire({
-          title:'Exito!',
-          text:'El cliente fue creado correctamente',
-          icon:'success',
-          confirmButtonColor: 'grey',
-          confirmButtonText: 'OK',
-        });
-        setIsLoading(false);
-        navigate('/');
-      }
-      if ( resp.status === 400 ) {
-        setIsLoading(false);
-        return Swal.fire(
-          'Error!',
-          'Se produjo un error, por favor verifique los datos ingresados',
-          'error'
-        ).then(navigate(-1));
-
-
-      }
-     };
+    };
 
     const handleCancel = () => {
       setIsLoading(false);
-      navigate(-1);
+      navigate('/');
     };
 
     const handleDelete = async () => {
@@ -84,7 +97,6 @@ export const AddEditClientView = () => {
               'Se produjo un error de conexión, por favor reintente nuevamente',
               'error'
             );
-
           }
           if( resp.status === 200 ) {
             deleteClientContext( parseInt(id) );
@@ -96,115 +108,120 @@ export const AddEditClientView = () => {
               confirmButtonText: 'OK',
             });
             setIsLoading(false);
-            navigate(-1);
+            navigate('/');
           }
           if ( resp.status === 404 ) {
             setIsLoading(false);
             return Swal.fire(
               'Error!',
-              'Se produjo un error, por favor reintente nuevamente',
+              'No se encontro el recurso solicitado',
               'error'
             ).then(navigate(-1));
-
-
           }
         }
       })
     };
 
     useEffect( () => {
-      if ( id !== undefined ) {  
-      ( async () => {
-        
-          const {data} = await getClientById( id ); 
-          setValue('name', data.name);
-          setValue('lastname', data.lastname);
-          setValue('sexo', data.gender);
-          setValue('phone', data.phone);
-          setValue('dni', data.dni);
-      } )();
-    }
-  }, [] );
-    
+      if ( !isCreateMode ) {    
+        ( async () => {          
+            const {data} = await getClientById( id ); 
+            setValue('name', data.name);
+            setValue('lastname', data.lastname);
+            setValue('gender', data.gender);
+            setValue('phone', data.phone);
+            setValue('dni', data.dni);
+
+            setTitle(`Editar Cliente ${data.id}: ${data.name} ${data.lastname}`);
+        } )();
+      }
+    }, [] );
      
     return (
-      <div className="container border shadow mt-5 w-50">
-        <form onSubmit={handleSubmit(onSubmit)}>
+      <div className='container mt-3'>
+        <h2>{ title }</h2>
+        <hr></hr>
+        <div className="container border shadow mt-5 w-50">
+          
+          <form onSubmit={handleSubmit(onSubmit)}>
 
-          <div className="row justify-content-md-center mt-3">
-            <div className="col-md-auto form-group m-2">
-              <label>Nombre</label>
-              <input
-                name='name'
-                {...register("name")} 
-                type="text" 
-                className="form-control"/>
+            <div className="row justify-content-md-center mt-3">
+              <div className="col-md-auto form-group m-2">
+                <label>Nombre</label>
+                <input
+                  name='name'
+                  {...register("name")} 
+                  type="text" 
+                  className="form-control"/>
+              </div>
+
+              <div className="col-md-auto form-group m-2">
+                <label>Apellido</label>
+                <input 
+                  name='lastname'
+                  {...register("lastname")}
+                  type="text" 
+                  className="form-control"
+                />
+              </div>
             </div>
 
-            <div className="col-md-auto form-group m-2">
-              <label>Apellido</label>
-              <input 
-                name='lastname'
-                {...register("lastname")}
-                type="text" 
-                className="form-control"
-              />
+            <div className="row justify-content-md-center">
+
+              <div className="col form-group m-2">
+                <label>Sexo</label>
+                <select 
+                  name='gender'
+                  className="form-control" 
+                  {...register("gender")}
+                >                  
+                  { validGenders.map( ( value ) => (
+                      <option key={ value } value={ value }>{ value }</option>
+                  ))}                  
+                </select>
+
+              </div> 
+
+              <div className="col-sm-auto form-group m-2">
+                <label>Numero de telefono</label>
+                <input
+                  name='phone'
+                  {...register("phone")} 
+                  type="text" 
+                  className="form-control"
+                />
+              </div>
+
+              <div className="col-md form-group m-2">
+                <label>DNI</label>
+                <input
+                  name='dni'
+                  {...register("dni")} 
+                  type="text" 
+                  className="form-control"/>
+              </div>
+
             </div>
-          </div>
-
-          <div className="row justify-content-md-center">
-
-            <div className="col form-group m-2">
-              <label>Sexo</label>
-              <select 
-                className="form-control" 
-                {...register("gender")}
-              >
-                <option value="Masculino">Masculino</option>
-                <option value="Femenino">Femenino</option>
-                <option value="Otro">Otro</option>
-              </select>
-
-            </div> 
-
-            <div className="col-sm-auto form-group m-2">
-              <label>Numero de telefono</label>
-              <input
-                name='phone'
-                {...register("phone")} 
-                type="text" 
-                className="form-control"
-              />
+          
+            <div className="row d-flex justify-content-around">
+              {
+                ( !isCreateMode ) 
+                  ? <div className="col-lg form-group">
+                      <button disabled={isLoading} onClick={ () => handleDelete() } type="button" className="btn btn-danger m-3">Eliminar</button>
+                    </div>
+                  : null
+              }
+              
+              <div className="col-md-auto form-group">
+              <button disabled={isLoading} className="btn btn-outline-success m-3">Guardar</button>
+              <button disabled={isLoading} onClick={handleCancel} type="button" className="btn btn-secondary m-3">Cancelar</button>
+              </div>
             </div>
 
-            <div className="col-md form-group m-2">
-              <label>DNI</label>
-              <input
-                name='dni'
-                {...register("dni")} 
-                type="text" 
-                className="form-control"/>
-            </div>
+          </form>
+        </div>
 
-          </div>
-         
-
-          <div className="row d-flex justify-content-around">
-            {
-              ( id !== undefined ) 
-                ? <div className="col-lg form-group">
-                    <button disabled={isLoading} onClick={ () => handleDelete() } type="button" className="btn btn-danger m-3">Eliminar</button>
-                  </div>
-                : null
-            }
-            
-            <div className="col-md-auto form-group">
-            <button disabled={isLoading} className="btn btn-outline-success m-3">Guardar</button>
-            <button disabled={isLoading} onClick={handleCancel} type="button" className="btn btn-secondary m-3">Cancelar</button>
-            </div>
-          </div>
-
-        </form>
       </div>
+      
     );
 }
