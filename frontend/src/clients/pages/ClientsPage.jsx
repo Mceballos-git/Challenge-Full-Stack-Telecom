940b4e2039a320d2d0aaeae1ee011dde376eb9a5
NanasoDev <mariano.ceballos@gmail.com>
2023-07-18 20:22:22 -0300

    Add: Render LoadingPage on start app

--- frontend/src/clients/pages/ClientsPage.jsx
+++ frontend/src/clients/pages/ClientsPage.jsx
@@ -3,18 +3,21 @@ import { useContext } from 'react';
 import { ClientsContext } from '../../context/ClientsContext';
 import { NoClientsView } from '../views/NoClients.View';
 import { ClientsListView } from '../views/ClientsListView';
+import LoadingSpinner from '../../ui/spinner';
 
 
 export const ClientsPage = () => {
 
-  const { clients } = useContext( ClientsContext );
+  const { clients, isLoading } = useContext( ClientsContext );
 
   return (
-    
     <>
-      {(clients.length === 0 ) 
-        ? <NoClientsView/>
-        : <ClientsListView clients = { clients } />}
+        { (isLoading) 
+            ? <LoadingSpinner/>
+            : (clients.length === 0 ) 
+              ? <NoClientsView/>
+              : <ClientsListView clients = { clients }/>
+        }        
     </>
   )
 }
