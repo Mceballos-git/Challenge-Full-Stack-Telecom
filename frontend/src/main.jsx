45369d7169c61255d0bc55b1073bf28535581dd8
NanasoDev <mariano.ceballos@gmail.com>
2023-07-18 20:21:21 -0300

    Fix: Rename RootPage Filename

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { createBrowserRouter,  RouterProvider} from 'react-router-dom';
-import RootPage from './clients/views/root';
+import RootPage from './clients/pages/RootPage';
 import ErrorPage from './clients/pages/ErrorPage';
 import { ClientsPage } from './clients/pages/ClientsPage';
 
