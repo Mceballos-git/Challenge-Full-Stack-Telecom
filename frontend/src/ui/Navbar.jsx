b1a84836cfa449645b88f16707fa23a7a36934e2
NanasoDev <mariano.ceballos@gmail.com>
2023-07-20 07:48:48 -0300

    Add: Indentation & disable search on Create

--- frontend/src/ui/Navbar.jsx
+++ frontend/src/ui/Navbar.jsx
@@ -1,14 +1,19 @@
+import { useContext } from 'react';
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { useContext } from 'react';
 import { getClientByDni } from '../../api/client';
 import { ClientsContext } from '../context/ClientsContext';
 
 
 export const Navbar = () => {
 
+  const { 
+  setSearchedClient, 
+  isLoading, 
+  setIsLoading, 
+  isCreatingOrEditingClient } = useContext( ClientsContext );
+
   const { register, handleSubmit, setValue, formState: { errors } } = useForm();
-  const { setSearchedClient, isLoading, setIsLoading } = useContext( ClientsContext );
 
   const patterns = {
     numbers: /^[0-9]+$/i
@@ -36,40 +41,54 @@ export const Navbar = () => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-2">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="/">Clients App</a>
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <Link onClick={deleteSearchedClients} className="border border-info rounded m-2 nav-link" to={`/`}>Listado de clientes</Link>
-          </li>
-          <li className="nav-item active">
-            <Link reloadDocument className="border border-info rounded m-2 nav-link" to={`/create`}>Agregar cliente</Link>
-          </li>
-        </ul>
-        <form onSubmit={handleSubmit(onSubmit)} className="d-flex" role="search">
-          <input 
-            name='dni'
-            className={`form-control me-2 ${errors.dni ? "is-invalid" : ""}`} 
-            type="text" 
-            placeholder="Buscar por DNI"
-            {...register("dni", { 
-              pattern: {
-                value: patterns.numbers
-              }
-            })}
-          />
-          <div id="dni" className="invalid-feedback">Sólo números</div>
-          { isLoading ? (
-           <button className="btn btn-outline-info" disabled>
-            {/* <span class="spinner-border spinner-border-sm"></span> */}
-            Cargando...
-            </button>
-          ) : (
-            <button className="btn btn-outline-info" type="submit">Buscar</button>
-          )}
-      </form>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Clients App</a>
+
+          <ul className="navbar-nav">
+            <li className="btn btn-sm nav-item active">
+              <Link 
+                onClick={deleteSearchedClients} 
+                className="border border-info rounded m-2 nav-link" 
+                to={`/`}
+              >
+                Listado de clientes
+              </Link>
+            </li>
+            <li className="btn btn-sm nav-item active">
+              <Link 
+                hidden={ isCreatingOrEditingClient } 
+                reloadDocument 
+                className="border border-info rounded m-2 nav-link" 
+                to={`/create`}
+              >
+                Agregar cliente
+              </Link>
+            </li>
+          </ul>
+
+          <form onSubmit={handleSubmit(onSubmit)} className="d-flex" role="search">
+            <input
+              disabled={isCreatingOrEditingClient}
+              name='dni'
+              className={`form-control me-2 ${errors.dni ? "is-invalid" : ""}`} 
+              type="text" 
+              placeholder={`${isCreatingOrEditingClient ? 'Búsqueda desactivada' : 'Buscar por DNI'}`}
+              {...register("dni", { 
+                pattern: {
+                  value: patterns.numbers
+                }
+              })}
+            />
+            <div id="dni" className="invalid-feedback m-2">Sólo números</div>
+            { isLoading ? (
+              <button disabled={isCreatingOrEditingClient} className="btn btn-outline-info btn-sm">
+                Cargando...
+              </button>
+            ) : (
+              <button disabled={isCreatingOrEditingClient} className="btn btn-outline-info btn-sm" type="submit">Buscar</button>
+            )}
+          </form>
       </div>
-      
     </nav>
   )
 }
